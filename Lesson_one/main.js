d197ee55239816fff61723d00974ff60f17d7619
quang950 <quanghi176@gmail.com>
2026-02-04 20:58:16 +0700

    day_1
    study at f8 to lesson 17 (done)

---
+++ Lesson_one/main.js
@@ -0,0 +1,37 @@
+// variable = var
+// var fullName = "lỗi";
+// var content = " sai rồi má ";
+// alert(fullName); // thông báo đơn giản
+// alert(content);
+
+// console.log(fullName)
+
+/** các loại toán tử 
+     
+ */
+var a = 1;
+var b = 2;
+
+var bool = a > b;
+console.log(bool);
+
+// các kiểu dữ liệu PHỨC TẠP trong js
+// Funtion
+var myFuntion = function () {
+  alert("nice");
+};
+
+// myFuntion();
+
+// object types
+
+var myObiect = {
+  name: " quang",
+  age: 19,
+  address: "dong nai",
+};
+
+var myArray = ["js", "java", "c++"];
+for (let i = 0 ; i < myArray.length ; i ++){
+    console.log(myArray[1]);
+}
